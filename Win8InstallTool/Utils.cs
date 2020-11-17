11d43867438fbf886f7c8e7950ea0bab85e91cd4
Kaciras <Kaciras@protonmail.com>
2020-11-17 20:42:34 +0800

    Expand enviorment variables on ExtractStringFromDLL()

--- Win8InstallTool/Utils.cs
+++ Win8InstallTool/Utils.cs
@@ -8,59 +8,60 @@ using System.Text;
 
 namespace Win8InstallTool
 {
-	internal static class Utils
-	{
-		public static void ForEach<T>(this IEnumerable<T> ienum, Action<T> action)
-		{
-			foreach (var item in ienum) action(item);
-		}
+    internal static class Utils
+    {
+        public static void ForEach<T>(this IEnumerable<T> ienum, Action<T> action)
+        {
+            foreach (var item in ienum) action(item);
+        }
 
-		/// <summary>
-		/// 解析类似 "@shell32.dll,-1#immutable1" 这样的字符串，并读取其引用的DLL中的字符串资源。
-		/// </summary>
-		/// <param name="string">字符串引用</param>
-		/// <returns>字符串资源</returns>
-		public static string ExtractStringFromDLL(string @string)
-		{
-			if (@string[0] == '@')
-			{
-				@string = @string.Substring(1);
-			}
-			var splited = @string.Split(',');
-			var num = Math.Abs(int.Parse(splited[1]));
-			return ExtractStringFromDLL(splited[0], num);
-		}
+        /// <summary>
+        /// 解析类似 "@shell32.dll,-1#immutable1" 这样的字符串，并读取其引用的DLL中的字符串资源。
+        /// </summary>
+        /// <param name="string">字符串引用</param>
+        /// <returns>字符串资源</returns>
+        public static string ExtractStringFromDLL(string @string)
+        {
+            if (@string[0] == '@')
+            {
+                @string = @string.Substring(1);
+            }
+            var splited = @string.Split(',');
+            var num = Math.Abs(int.Parse(splited[1]));
+            return ExtractStringFromDLL(splited[0], num);
+        }
 
-		/// <summary>
-		/// 从 Windows 动态链接库文件里读取字符串资源。
-		/// </summary>
-		/// <param name="file">DLL文件</param>
-		/// <param name="number">资源索引，不能是负数</param>
-		/// <returns>字符串资源</returns>
-		public static string ExtractStringFromDLL(string file, int number)
-		{
-			var lib = LoadLibrary(file);
+        /// <summary>
+        /// 从 Windows 动态链接库文件里读取字符串资源。
+        /// </summary>
+        /// <param name="file">DLL文件</param>
+        /// <param name="number">资源索引，不能是负数</param>
+        /// <returns>字符串资源</returns>
+        public static string ExtractStringFromDLL(string file, int number)
+        {
+            file = Environment.ExpandEnvironmentVariables(file);
+            var lib = LoadLibrary(file);
 
-			var code = Marshal.GetLastWin32Error();
-			if (code != 0)
-			{
-				throw new SystemException($"无法加载{file}，错误代码:{code}");
-			}
+            var code = Marshal.GetLastWin32Error();
+            if (code != 0)
+            {
+                throw new SystemException($"无法加载{file}，错误代码:{code}");
+            }
 
-			var buffer = new StringBuilder(2048);
-			LoadString(lib, number, buffer, buffer.Capacity);
-			FreeLibrary(lib);
-			return buffer.ToString();
-		}
+            var buffer = new StringBuilder(2048);
+            LoadString(lib, number, buffer, buffer.Capacity);
+            FreeLibrary(lib);
+            return buffer.ToString();
+        }
 
-		[DllImport("kernel32.dll", SetLastError = true)]
-		static extern IntPtr LoadLibrary(string lpFileName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr LoadLibrary(string lpFileName);
 
-		[DllImport("user32.dll")]
-		static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
+        [DllImport("user32.dll")]
+        static extern int LoadString(IntPtr hInstance, int ID, StringBuilder lpBuffer, int nBufferMax);
 
-		[DllImport("kernel32.dll")]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		static extern bool FreeLibrary(IntPtr hModule);
-	}
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FreeLibrary(IntPtr hModule);
+    }
 }
