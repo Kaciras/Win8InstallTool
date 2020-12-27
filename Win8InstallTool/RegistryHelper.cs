33e37cccd7b9b3d7fba6883d2cd9c091aa29c5fd
Kaciras <Kaciras@protonmail.com>
2020-12-27 13:21:09 +0800

    remove unused functino RegistryHelper.ElevatePermission()

--- Win8InstallTool/RegistryHelper.cs
+++ Win8InstallTool/RegistryHelper.cs
@@ -108,44 +108,5 @@ namespace Win8InstallTool
 			using var subKey = key.OpenSubKey(name);
 			return subKey != null;
 		}
-
-		/// <summary>
-		/// 尽管程序要求以管理员身份运行，但有些注册表键仍然没有修改权限，故需要添加一下权限。
-		/// 可以使用using语法来自动还原权限：
-		/// <code>
-		///		using (RegistryHelper.ElevatePermission(key)) { ... }
-		/// </code>
-		/// </summary>
-		/// <param name="key">键</param>
-		/// <returns>一个可销毁对象，在销毁时还原键的权限</returns>
-		public static IDisposable ElevatePermission(RegistryKey key)
-		{
-			var old = key.GetAccessControl();
-
-			var accessControl = key.GetAccessControl();
-			var rule = new RegistryAccessRule(WindowsIdentity.GetCurrent().User, RegistryRights.FullControl, AccessControlType.Allow);
-			accessControl.AddAccessRule(rule);
-			key.SetAccessControl(accessControl);
-
-			return new TemporaryElevateSession(key, old);
-		}
-
-		internal readonly struct TemporaryElevateSession : IDisposable
-		{
-			readonly RegistryKey key;
-			readonly RegistrySecurity accessControl;
-
-			public TemporaryElevateSession(RegistryKey key, RegistrySecurity accessControl)
-			{
-				this.key = key;
-				this.accessControl = accessControl;
-			}
-
-			public void Dispose()
-			{
-				// TODO: key被删了咋办
-				key.SetAccessControl(accessControl);
-			}
-		}
 	}
 }
