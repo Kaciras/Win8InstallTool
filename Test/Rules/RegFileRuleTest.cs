9ce75c105a81ec1b2019012064a24e5226927c44
Kaciras <Kaciras@protonmail.com>
2021-11-10 17:38:47 +0800

    add a test for RegFileRule.Optimize

--- Test/Rules/RegFileRuleTest.cs
+++ Test/Rules/RegFileRuleTest.cs
@@ -1,6 +1,6 @@
-﻿using System.IO;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Win32;
+using System.IO;
 using static System.Environment;
 
 namespace Win8InstallTool.Rules;
@@ -8,44 +8,65 @@ namespace Win8InstallTool.Rules;
 [TestClass]
 public sealed class RegFileRuleTest
 {
-    [ClassInitialize]
-    public static void ImportTestData(TestContext _)
-    {
-        RegistryHelper.Import(@"Resources\Kinds.reg");
-    }
+	[TestInitialize]
+	public void ImportTestData()
+	{
+		RegistryHelper.Import(@"Resources\Kinds.reg");
+	}
 
-    [ClassCleanup]
-    public static void Cleanup()
-    {
-        Registry.CurrentUser.DeleteSubKeyTree("_Test_Kinds");
-    }
+	[TestCleanup]
+	public void Cleanup()
+	{
+		Registry.CurrentUser.DeleteSubKeyTree("_Test_Kinds", false);
+		Registry.CurrentUser.DeleteSubKeyTree("_Test_Import", false);
+	}
 
-    [TestMethod]
-    public void Check()
-    {
-        var content = File.ReadAllText(@"Resources\Kinds.reg");
-        var rule = new RegFileRule("test", "test", content);
-        Assert.IsFalse(rule.Check());
-    }
+	[TestMethod]
+	public void CheckNoNeeded()
+	{
+		var content = File.ReadAllText(@"Resources\Kinds.reg");
+		var rule = new RegFileRule("test", "test", content);
+		Assert.IsFalse(rule.Check());
+	}
 
-    /// <summary>
-    /// 该测试不对应代码，仅用于验证 Registry.GetValue 返回值的类型。
-    /// </summary>
-    [TestMethod]
-    public void MyTestMethod()
-    {
-        using var key = Registry.CurrentUser.OpenSubKey("_Test_Kinds");
+	[TestMethod]
+	public void Check()
+	{
+		var content = File.ReadAllText(@"Resources\ImportTest.reg");
+		var rule = new RegFileRule("test", "test", content);
+		Assert.IsTrue(rule.Check());
+	}
 
-        Assert.AreEqual("文字文字", key.GetValue(""));
+	[TestMethod]
+	public void Optimize()
+	{
+		var content = File.ReadAllText(@"Resources\ImportTest.reg");
+		var rule = new RegFileRule("test", "test", content);
 
-        Assert.AreEqual(0x123, key.GetValue("Dword"));
+		rule.Optimize();
 
-        Assert.AreEqual(0x666888L, key.GetValue("Qword"));
+		var value = Registry.GetValue(@"HKEY_CURRENT_USER\_Test_Import\Key", "StringValue", null);
+		Assert.AreEqual("中文内容", value);
+	}
 
-        CollectionAssert.AreEqual(new string[] { "Str0", "Str1" }, (string[])key.GetValue("Multi"));
+	/// <summary>
+	/// 该测试不对应代码，仅用于验证 Registry.GetValue 返回值的类型。
+	/// </summary>
+	[TestMethod]
+	public void MyTestMethod()
+	{
+		using var key = Registry.CurrentUser.OpenSubKey("_Test_Kinds");
 
-        Assert.AreEqual(GetFolderPath(SpecialFolder.UserProfile), key.GetValue("Expand"));
+		Assert.AreEqual("文字文字", key.GetValue(""));
 
-        CollectionAssert.AreEqual(new byte[] { 0xFA, 0x51, 0x6F, 0x89 }, (byte[])key.GetValue("Binary"));
-    }
+		Assert.AreEqual(0x123, key.GetValue("Dword"));
+
+		Assert.AreEqual(0x666888L, key.GetValue("Qword"));
+
+		CollectionAssert.AreEqual(new string[] { "Str0", "Str1" }, (string[])key.GetValue("Multi"));
+
+		Assert.AreEqual(GetFolderPath(SpecialFolder.UserProfile), key.GetValue("Expand"));
+
+		CollectionAssert.AreEqual(new byte[] { 0xFA, 0x51, 0x6F, 0x89 }, (byte[])key.GetValue("Binary"));
+	}
 }
