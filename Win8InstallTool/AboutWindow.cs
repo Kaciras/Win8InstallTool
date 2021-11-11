5e08a6edd57694b54cc05940ee8923ed563b333e
Kaciras <Kaciras@protonmail.com>
2021-11-11 14:46:51 +0800

    bump version

--- Win8InstallTool/AboutWindow.cs
+++ Win8InstallTool/AboutWindow.cs
@@ -11,7 +11,7 @@ public partial class AboutWindow : Form
 		InitializeComponent();
 
 		var version = Assembly.GetExecutingAssembly().GetName().Version;
-		versionLabel.Text = $"版本 {version.ToString(3)}，更新于 2021-2-26";
+		versionLabel.Text = $"版本 {version.ToString(3)}，更新于 2021-11-11";
 	}
 
 	private void CloseButton_Click(object sender, EventArgs e)
